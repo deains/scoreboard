c8ff33bfb794307a9208108a127a271ecea0b864
Damian Posener <750763+deains@users.noreply.github.com>
2021-08-12 11:34:07 +0100

    Update prod deploy

--- env_prod/settings.py
+++ env_prod/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-$k5ep=$qq=11@8vf_0y1%k*e-t8+%wd7v#-o_5a*e@n+gwxrg5'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
